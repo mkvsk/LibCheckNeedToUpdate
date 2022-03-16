9e23d7efca4f95b287f0cc858f3ff35cca56515a
mkvsk <tanysha6@me.com>
2022-03-16 13:58:00 +0200

    delete catch(ex)

--- LibCheckNeedToUpdate/Version.cs
+++ LibCheckNeedToUpdate/Version.cs
@@ -20,63 +20,59 @@
 			string x2 = string.Empty;
 			string y2 = string.Empty;
 			string z2 = string.Empty;
-			
-			try
+
+			char[] vArr1 = currentVersion.ToCharArray();
+			char[] vArr2 = otherVersion.ToCharArray();
+
+			int counter1 = 0;
+			int counter2 = 0;
+
+			foreach (char c in vArr1)
+			{
+				if (c != ('.'))
+				{
+					if (counter1 == 0) { x1.Append(vArr1[c]); }
+					if (counter1 == 1) { y1.Append(vArr1[c]); }
+					if (counter1 == 2) { z1.Append(vArr1[c]); }
+				}
+				if (c == ('.'))
+				{
+					counter1++;
+				}
+			}
+
+			foreach (char c in vArr2)
+			{
+				if (c != ('.'))
+				{
+					if (counter2 == 0) { x2.Append(vArr2[c]); }
+					if (counter2 == 1) { y2.Append(vArr2[c]); }
+					if (counter2 == 2) { z2.Append(vArr2[c]); }
+				}
+				if (c == ('.'))
+				{
+					counter2++;
+				}
+			}
+
+			int xV1 = int.Parse(x1);
+			int yV1 = int.Parse(y1);
+			int zV1 = int.Parse(z1);
+
+			int xV2 = int.Parse(x2);
+			int yV2 = int.Parse(y2);
+			int zV2 = int.Parse(z2);
+
+			if ((z1 is not null || y1 is not null || x1 is not null) && (z2 is not null || y2 is not null || x2 is not null))
+			{
+				IsNeedToUpdate(xV1, yV1, zV1, xV2, yV2, zV2);
+			}
+			else
             {
-				char[] vArr1 = currentVersion.ToCharArray();
-				char[] vArr2 = otherVersion.ToCharArray();
-
-				int counter1 = 0;
-				int counter2 = 0;
-
-				foreach (char c in vArr1)
-				{
-					if (c != ('.'))
-					{
-						if (counter1 == 0) { x1.Append(vArr1[c]); }
-						if (counter1 == 1) { y1.Append(vArr1[c]); }
-						if (counter1 == 2) { z1.Append(vArr1[c]); }
-					}
-					if (c == ('.'))
-					{
-						counter1++;
-					}
-				}
-
-				foreach (char c in vArr2)
-				{
-					if (c != ('.'))
-					{
-						if (counter2 == 0) { x2.Append(vArr2[c]); }
-						if (counter2 == 1) { y2.Append(vArr2[c]); }
-						if (counter2 == 2) { z2.Append(vArr2[c]); }
-					}
-					if (c == ('.'))
-					{
-						counter2++;
-					}
-				}
-			}
-			catch
-			{	
-				Console.WriteLine("Input versions in right format x.x.x"); 
-			}
-			finally
-            {
-				int xV1 = int.Parse(x1);
-				int yV1 = int.Parse(y1);
-				int zV1 = int.Parse(z1);
-
-				int xV2 = int.Parse(x2);
-				int yV2 = int.Parse(y2);
-				int zV2 = int.Parse(z2);
-
-				if ((z1 is not null || y1 is not null || x1 is not null) && (z2 is not null || y2 is not null || x2 is not null))
-				{
-					IsNeedToUpdate(xV1, yV1, zV1, xV2, yV2, zV2);
-				}
-			}
-        }
+				Console.WriteLine("Invalid data. Type version in right formar x.x.x");
+				Console.WriteLine();
+            }
+		}
 		 
 		public bool IsNeedToUpdate(int xV1, int yV1, int zV1, int xV2, int yV2, int zV2)
 		{	 
